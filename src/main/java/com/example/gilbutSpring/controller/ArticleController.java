c1268e0698915c66721cfcb8d46c157a1e08ed50
madogkwon <a93081077a@gmail.com>
2024-11-19 19:47:41 +0900

    Fetch single data by id

--- src/main/java/com/example/gilbutSpring/controller/ArticleController.java
+++ src/main/java/com/example/gilbutSpring/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.gilbutSpring.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -35,5 +39,16 @@ public class ArticleController {
     log.info(saved.toString());
     // System.out.println(saved.toString());
     return "";
+  }
+
+  @GetMapping("/articles/{id}")
+  public String show(@PathVariable Long id, Model model) {
+    log.info("id = " + id);
+    // 1. Fetch data by id
+    Article articleEntity = articleRepository.findById(id).orElse(null);
+    // 2. Add data to the model
+    model.addAttribute("article", articleEntity);
+    // 3. Return the view page
+    return "articles/show";
   }
 }
