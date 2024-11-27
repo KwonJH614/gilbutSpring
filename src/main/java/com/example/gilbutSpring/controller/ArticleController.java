d9c51b8d199db17aba9ac01daaa77f121131b3e2
madogkwon <a93081077a@gmail.com>
2024-11-27 23:32:52 +0900

    ~ 248 p. (delete)

--- src/main/java/com/example/gilbutSpring/controller/ArticleController.java
+++ src/main/java/com/example/gilbutSpring/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -85,5 +86,20 @@ public class ArticleController {
     if (target != null) { articleRepository.save(article); }
     // 3. Redirect to the edit result page
     return "redirect:/articles/" + article.getId();
+  }
+
+  @GetMapping("/articles/{id}/delete")
+  public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+    log.info("삭제 요청");
+    // 1. 삭제 대상 사져오기
+    Article target = articleRepository.findById(id).orElse(null);
+    log.info(target.toString());
+    // 2. 대상 엔티티 삭제
+    if (target != null) {
+      articleRepository.delete(target);
+      rttr.addFlashAttribute("msg", "삭제됐습니다");
+    }
+    // 3. 결과 페이지로 리다이렉트
+    return "redirect:/articles";
   }
 }
