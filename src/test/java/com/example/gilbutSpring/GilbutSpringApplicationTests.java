50879e17c55fb3ac6fa10006fd5ac85b2bd22864
madogkwon <a93081077a@gmail.com>
2024-11-28 23:41:58 +0900

    ~ 277 p. (CRUD, SQL QUERY)

---
+++ src/test/java/com/example/gilbutSpring/GilbutSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gilbutSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilbutSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
