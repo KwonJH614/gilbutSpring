ac243b5f5994764134372cee14efd55cf1e70a53
madogkwon <a93081077a@gmail.com>
2024-12-08 00:07:05 +0900

    ~ 350 p. SERVICE (PATCH, DELETE)

--- src/main/java/com/example/gilbutSpring/api/ArticleApiController.java
+++ src/main/java/com/example/gilbutSpring/api/ArticleApiController.java
@@ -43,17 +43,12 @@ public class ArticleApiController {
             ResponseEntity.status(HttpStatus.OK).body(updated) :
             ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
   }
-//  // DELETE
-//  @DeleteMapping("/api/articles/{id}")
-//  public ResponseEntity<Article> delete(@PathVariable Long id) {
-//    // 1. 대상 찾기
-//    Article target = articleRepository.findById(id).orElse(null);
-//    // 2. 잘못된 요청 처리
-//    if (target == null) {
-//      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//    }
-//    // 3. 대상 삭제
-//    articleRepository.delete(target);
-//    return ResponseEntity.status(HttpStatus.OK).body(null);
-//  }
+  // DELETE
+  @DeleteMapping("/api/articles/{id}")
+  public ResponseEntity<Article> delete(@PathVariable Long id) {
+    Article deleted = articleService.delete(id);
+    return (deleted != null) ?
+            ResponseEntity.status(HttpStatus.OK).build() :
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+  }
 }
