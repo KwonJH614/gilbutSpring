b9f10eda9b17fdf3f7e1abfe649fa1d4b45bd70e
madogkwon <a93081077a@gmail.com>
2024-12-09 20:25:09 +0900

    ~ 387 p. TestCode(Read, Create, Update)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article(title, content) values ( '111111', '가가가가');
-INSERT INTO article( title, content) values ( '222222', '나나나나');
-INSERT INTO article( title, content) values ( '333333', '다다다다');
+INSERT INTO article(title, content) values ( '가가가가', '1111');
+INSERT INTO article( title, content) values ( '나나나나', '2222');
+INSERT INTO article( title, content) values ( '다다다다', '3333');
