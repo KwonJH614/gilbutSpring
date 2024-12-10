fac841f3579d79666ed8d6e3a0a0492005aca02b
madogkwon <a93081077a@gmail.com>
2024-12-10 20:25:29 +0900

    ~ 410 p. Comment Entity, Repository

---
+++ src/main/java/com/example/gilbutSpring/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.example.gilbutSpring.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @ManyToOne
+  @JoinColumn(name = "article_id")
+  private Article article;
+  @Column
+  private String nickname;
+  @Column
+  private String body;
+}
---
+++ src/main/java/com/example/gilbutSpring/repository/CommentRepository.java
@@ -0,0 +1,20 @@
+package com.example.gilbutSpring.repository;
+
+import com.example.gilbutSpring.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+// JpaRepository <대상 엔티티, 대표키의 타입>
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+  // 특정 게시글의 모든 댓글 조회
+  @Query (value =
+          "SELECT * " +
+          "FROM comment " +
+          "WHERE article_id = :articleId",
+          nativeQuery = true)
+  List<Comment> findByArticleId(Long articleId);
+  // 특정 닉네임의 모든 댓글 조회
+  List<Comment> findByNickName(String nickname);
+}
